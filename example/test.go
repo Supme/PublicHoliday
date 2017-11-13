b14817f7f76cf73936b7ee8f4f0c7a3ca04c0e39
supme <supmea@gmail.com>
2017-11-13 18:13:17 +0300

    update readme and example

--- example/test.go
+++ example/test.go
@@ -15,42 +15,49 @@ func main() {
 
 	time.Sleep(time.Duration(time.Second * 2))
 
-	dw, err := time.Parse("02.01.2006", "18.01.2004")
-	if err != nil {
-		panic(err)
-	}
+	dw := dateFromString( "18.01.2004")
+	// праздничный или выходной день?
 	rw, err :=ph.IsWeekend(dw)
 	chkErr(err)
 	fmt.Println("Result weekend:", rw)
 
-	ds, err := time.Parse("02.01.2006", "30.04.1999")
-	if err != nil {
-		panic(err)
-	}
+	ds := dateFromString( "30.04.1999")
+	// сокращённый предпраздничный?
 	rs, err :=ph.IsShortDay(ds)
 	chkErr(err)
 	fmt.Println("Result shortday:", rs)
 
+	// изменить интервал обновления кэша (по умолчанию 24 часа)
 	ph.SetCacheTime(time.Second * 1)
 
 	time.Sleep(time.Duration(time.Second * 2))
 
-	d, err := time.Parse("02.01.2006", "01.01.2018")
-	if err != nil {
-		panic(err)
-	}
+	d := dateFromString("01.01.2018")
+	// всего рабочих дней
 	wd, err := ph.WorkingDays(d)
 	chkErr(err)
+	// всего праздничных и выходных дней
 	hd, err := ph.Holidays(d)
 	chkErr(err)
+	// количество рабочих часов при 40-часовой рабочей неделе
 	h40, err := ph.WorkingHours40hWeek(d)
 	chkErr(err)
+	// количество рабочих часов при 36-часовой рабочей неделе
 	h36, err := ph.WorkingHours36hWeek(d)
 	chkErr(err)
+	// количество рабочих часов при 24-часовой рабочей неделе
 	h24, err := ph.WorkingHours24hWeek(d)
 	chkErr(err)
 
 	fmt.Printf("wd: %d, hd: %d, h40: %.01f, h36: %.01f, h24: %.01f\n", wd, hd, h40, h36, h24)
+}
+
+func dateFromString(str string) time.Time {
+	d, err := time.Parse("02.01.2006", str)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
 
 func chkErr(err error) {
